9791cc281df6a8e5ce381822fe5dd9356fffa9f1
Yasir Elamin <elam0012@algonquinlive.com>
2022-12-03 16:48:12 -0500

    Added the core Components

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, Image, Button, TextInput, FlatList, StatusBar, ActivityIndicator } from 'react-native';
+import {useState, useEffect} from "react"
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 export default function App() {
+
+  const [input, setInput] = useState(null); // to catch the text input
+  const[data, setData] = useState(null)
+
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
+
+  function buttonPressed() { // to handle the press button to display the input content
+    alert(input)
+  }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+    .then(response => response.json())
+    .then(data => setData(data))
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <Image source={require("./assets/profile.jpg")} style={styles.image}/>
+        <Text>Welcome Back to the Best App Ever!</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setInput}
+          value={input}
+          placeholder="useless placeholder"
+        />
+        <Button title='Button' onPress={buttonPressed}/>
+        { data? 
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        /> :
+        <ActivityIndicator size="large" color="red" style={styles.indicator}/>
+        }
+        <StatusBar backgroundColor="red"/>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +58,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 100,
+    height: 100,
+  },
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  indicator: {
+  }
 });
